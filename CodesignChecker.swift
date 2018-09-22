cf18e209f153d4c932c741e5452ca221dd9191d5
Antti Tulisalo <suolapeikko@users.noreply.github.com>
2018-09-22 12:04:11 +0300

    Update CodesignChecker.swift

--- CodesignChecker.swift
+++ CodesignChecker.swift
@@ -5,84 +5,211 @@
 //  Created by Antti Tulisalo on 20/09/2018.
 //
 
-// Based loosely on Objectice-C code of this: https://github.com/NBICreator/NBICreator/blob/master/NBICreator/Helper/SNTCodesignChecker.m
-
-/*
- 
- Workflow in the user (Privileged helper) of this utility struct should go something like this:
- 
-    let checker = CodesignChecker()
-    let pid = connection.processIdentifier
- 
-    let certs1 = checker.prepareWithSelf()
-    let certs2 = checker.prepareWithPID(pid: pid)
-
-    let remoteApp = NSRunningApplication.init(processIdentifier: pid)
- 
-    // Then compare cert arrays and that remoteApp exists
-    if(remoteApp != nil && (certs1 == certs2)) {
-    return true
- */
-
 import Foundation
 import Security
 
+struct CodesignCheckerError : Error
+{
+    enum Category
+    {
+        case SecCodeCopySelf
+        case SecCodeCopyStaticCode
+        case SecCodeCopyGuestWithAttributes
+        case SecStaticCodeCreateWithPath
+        case SecCodeCopySigningInformation
+        case GenericError
+    }
+    
+    let type: Category
+    let description: String
+    let methodName: String
+    let fileName: String
+    let lineNumber: Int
+    
+    static func handle(error: CodesignCheckerError) -> String
+    {
+        let readableError = """
+        \nERROR - operation: [\(error.type)];
+        reason: [\(error.description)];
+        in method: [\(error.methodName)];
+        in file: [\(error.fileName)];
+        at line: [\(error.lineNumber)]\n
+        """
+        print(readableError)
+        return readableError
+    }
+    
+}
+
+
+// https://developer.apple.com/documentation/security/code_signing_services
 struct CodesignChecker {
 
-    // This is from ported code, I think not needed like this so defaults should be ok
-    //let secFlags = SecCSFlags.init(rawValue: kSecCSDoNotValidateResources | kSecCSCheckNestedCode)
+    let kSecCSDefaultFlags = SecCSFlags.init(rawValue: 0)
     
-    // Swift API lacks 'kSecCSDefaultFlags' AFAIK, but it should be zero anyway, so..
-    let secFlags = SecCSFlags.init(rawValue: 0)
+    let kSecCSCustomFlags = SecCSFlags.init(rawValue: kSecCSDoNotValidateResources | kSecCSCheckNestedCode)
     
-    func validateWithSecStaticCodeRef(codeRef: SecCode?) -> [Any] {
+    // https://developer.apple.com/documentation/security/1401695-seccodecopystaticcode
+    private func prepareSelf() throws -> SecStaticCode? {
         
-        NSLog("TypeID: \(CFGetTypeID(codeRef))")
-        NSLog("Static: \(SecStaticCodeGetTypeID())")
-        if(CFGetTypeID(codeRef) == SecStaticCodeGetTypeID()) { // For some reason this line always fails for me
-            NSLog("1")
-            if(SecStaticCodeCheckValidity(codeRef! as! SecStaticCode, SecCSFlags.init(rawValue: kSecCSBasicValidateOnly), nil) == errSecSuccess) {
-              NSLog("Signing is valid!")
-                // Continue to do the rest of the stuff (certificate array)
-            }
-        }
-        else {
-            NSLog("Jumps straight at here for some reason")
+        var secCodeSelf: SecCode?
+        var secStaticCode: SecStaticCode?
+        
+        var resultCode = SecCodeCopySelf(kSecCSDefaultFlags, &secCodeSelf)
+        
+        guard resultCode == errSecSuccess, let secCode = secCodeSelf else {
+            
+            throw CodesignCheckerError(type: .SecCodeCopySelf, description: String(describing: SecCopyErrorMessageString(resultCode, nil)), methodName: #function, fileName: #file, lineNumber: #line)
         }
 
-        // Returns an array of 'kSecCodeInfoCertificates'
-        return []
+        resultCode = SecCodeCopyStaticCode(secCode, kSecCSDefaultFlags, &secStaticCode)
+        
+        guard resultCode == errSecSuccess, secStaticCode != nil else {
+            
+            throw CodesignCheckerError(type: .SecCodeCopyStaticCode, description: String(describing: SecCopyErrorMessageString(resultCode, nil)), methodName: #function, fileName: #file, lineNumber: #line)
+        }
+
+        return secStaticCode
     }
-    
-    func prepareWithPID(pid: pid_t) -> [Any] {
 
-        var codeSelf: SecCode?
-        //var resultCode: OSStatus
+    // https://developer.apple.com/documentation/security/1395560-seccodecopyguestwithattributes
+    private func prepare(withPID pid: pid_t) throws -> SecStaticCode? {
         
-        let attributes = [
+        var secCodePID: SecCode?
+        var secStaticCode: SecStaticCode?
+
+        let kSecAttributes = [
             kSecGuestAttributePid : pid
         ]
-        
-        if(SecCodeCopyGuestWithAttributes(nil, attributes as CFDictionary, SecCSFlags.init(rawValue: 0), &codeSelf) == errSecSuccess) {
-            
-            _ = validateWithSecStaticCodeRef(codeRef: codeSelf)
+
+        var resultCode = SecCodeCopyGuestWithAttributes(nil, kSecAttributes as CFDictionary, kSecCSDefaultFlags, &secCodePID)
+
+        guard resultCode == errSecSuccess, let secCode = secCodePID else {
+
+            throw CodesignCheckerError(type: .SecCodeCopyGuestWithAttributes, description: String(describing: SecCopyErrorMessageString(resultCode, nil)), methodName: #function, fileName: #file, lineNumber: #line)
         }
         
-        // Returns an array of 'kSecCodeInfoCertificates'
-        return []
+        resultCode = SecCodeCopyStaticCode(secCode, kSecCSDefaultFlags, &secStaticCode)
+        
+        guard resultCode == errSecSuccess, secStaticCode != nil else {
+            
+            throw CodesignCheckerError(type: .SecCodeCopyStaticCode, description: String(describing: SecCopyErrorMessageString(resultCode, nil)), methodName: #function, fileName: #file, lineNumber: #line)
+        }
+        
+        return secStaticCode
+    }
+
+    // https://developer.apple.com/documentation/security/1396899-secstaticcodecreatewithpath
+    private func prepare(withURL url: URL) throws -> SecStaticCode? {
+        
+        var secStaticCode: SecStaticCode?
+        
+        let resultCode = SecStaticCodeCreateWithPath(url as CFURL, [], &secStaticCode)
+        
+        guard resultCode == errSecSuccess && secStaticCode != nil else {
+            
+            throw CodesignCheckerError(type: .SecStaticCodeCreateWithPath, description: String(describing: SecCopyErrorMessageString(resultCode, nil)), methodName: #function, fileName: #file, lineNumber: #line)
+        }
+        
+        return secStaticCode
+    }
+
+    // Checking the validity of IDs
+    private func isValid(secStaticCode: SecStaticCode) -> Bool {
+        
+        guard CFGetTypeID(secStaticCode) == SecStaticCodeGetTypeID() else {
+            return false
+        }
+        
+        let resultCode = SecStaticCodeCheckValidity(secStaticCode, kSecCSCustomFlags, nil)
+        
+        guard resultCode == errSecSuccess else {
+
+            return false
+        }
+        
+        return true
     }
     
-    func prepareWithSelf() -> [Any] {
+    // https://developer.apple.com/documentation/security/1395809-seccodecopysigninginformation
+    private func getCertificates(secStaticCode: SecStaticCode) throws -> [SecCertificate] {
         
-        var codeSelf: SecCode?
-        //var resultCode: OSStatus
+        var secCodeInfoCFDict:  CFDictionary?
+        let resultCode = SecCodeCopySigningInformation(secStaticCode, SecCSFlags(rawValue: kSecCSSigningInformation), &secCodeInfoCFDict)
 
-        if(SecCodeCopySelf(SecCSFlags.init(rawValue: 0), &codeSelf) == errSecSuccess) {
-            // Then should do this as well
-            //self.validateWithSecStaticCodeRef(codeRef: codeSelf)
+        guard resultCode == errSecSuccess, let secCodeInfo = secCodeInfoCFDict as? [String: Any] else {
+            
+            throw CodesignCheckerError(type: .SecCodeCopySigningInformation, description: String(describing: SecCopyErrorMessageString(resultCode, nil)), methodName: #function, fileName: #file, lineNumber: #line)
         }
         
-        // Returns an array of 'kSecCodeInfoCertificates'
-        return []
+        guard let secCertificates = secCodeInfo[kSecCodeInfoCertificates as String] as? [SecCertificate] else {
+            
+            throw CodesignCheckerError(type: .GenericError, description: "Failed to obtain certificates from the information dictionary", methodName: #function, fileName: #file, lineNumber: #line)
+        }
+        
+        return secCertificates
+    }
+    
+    public func getCertificatesSelf() throws -> [SecCertificate] {
+        
+        let secStaticCode: SecStaticCode?
+        var certificates: [SecCertificate] = []
+        
+        do {
+            try secStaticCode = prepareSelf()
+            
+            if(!isValid(secStaticCode: secStaticCode!)) {
+                throw CodesignCheckerError(type: .GenericError, description: "Validation failed", methodName: #function, fileName: #file, lineNumber: #line)
+            }
+            
+            try certificates = getCertificates(secStaticCode: secStaticCode!)
+        }
+        catch let error {
+            throw error
+        }
+        
+        return certificates
+    }
+    
+    public func getCertificates(forPID pid: pid_t) throws -> [SecCertificate] {
+        
+        let secStaticCode: SecStaticCode?
+        var certificates: [SecCertificate] = []
+        
+        do {
+            try secStaticCode = prepare(withPID: pid)
+            
+            if(!isValid(secStaticCode: secStaticCode!)) {
+                throw CodesignCheckerError(type: .GenericError, description: "Validation failed", methodName: #function, fileName: #file, lineNumber: #line)
+            }
+            
+            try certificates = getCertificates(secStaticCode: secStaticCode!)
+        }
+        catch let error {
+            throw error
+        }
+        
+        return certificates
+    }
+    
+    public func getCertificates(forURL url: URL) throws -> [SecCertificate] {
+        
+        let secStaticCode: SecStaticCode?
+        var certificates: [SecCertificate] = []
+        
+        do {
+            try secStaticCode = prepare(withURL: url)
+            
+            if(!isValid(secStaticCode: secStaticCode!)) {
+                throw CodesignCheckerError(type: .GenericError, description: "Validation failed", methodName: #function, fileName: #file, lineNumber: #line)
+            }
+            
+            try certificates = getCertificates(secStaticCode: secStaticCode!)
+        }
+        catch let error {
+            throw error
+        }
+        
+        return certificates
     }
 }
